f5257eb6592b883a1200b3c0a8f975debd60b89c
Marco Paulo Fernandes Gomes <marco.gomes@neotalentconclusion.com>
2026-08-24 15:31:16 +0100

    Use Python HTTP server instead of npx

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
-echo Starting Dungeon Maze on http://localhost:3000
+echo Starting Dungeon Maze on http://localhost:8000
 echo Press Ctrl+C to stop.
 echo.
 cd /d "%~dp0"
-npx -y http-server -p 3000 -c-1
+python -m http.server 8000
